ce5c29ac9221616832472ea400ace61c3596d36b
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-03-23 22:15:52 +0900

    基本動作確認

--- app/server/Store/LoginUsersStore.ts
+++ app/server/Store/LoginUsersStore.ts
@@ -1,16 +1,21 @@
-import DomainException from "server/Domain/Exception/DomainException";
+import Exception from "../Domain/Exception/Exception";
+import DomainException from "../Domain/Exception/DomainException";
 import User from "../Domain/User/User";
 export default {
     users: [] as unknown as User[],
     maxUserNum: 100,
     enqueue(user: User){
-        if(user.id && this.users.length < this.maxUserNum){
+        if(!user.id){
+            throw new  Exception('idの無いユーザーをストアに追加しようとしました。');
+        }
+        if(this.users.length < this.maxUserNum){
             if(!this.inUsers(user.id)){
                 this.users.push(user);
             }
             return true;
+        }else{
+            throw new DomainException('ログインユーザー数が上限を超えました。しばらくしてから再ログインしてください。');
         }
-        throw new DomainException('ログインユーザー数が上限を超えました。しばらくしてから再ログインしてください。');
     },
     dequeue(){
         return this.users.shift();
