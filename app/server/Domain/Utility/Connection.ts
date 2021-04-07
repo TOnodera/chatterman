38ab26abb71c82dd38c9d15100437f885b0c5d69
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-08 00:21:18 +0900

    トランザクション処理の後に値を配列で返せるように修正

--- app/server/Domain/Utility/Connection.ts
+++ app/server/Domain/Utility/Connection.ts
@@ -2,20 +2,25 @@ import { createPool } from 'mysql2/promise';
 import config from '../../config';
 
 const mySqlConnector = createPool(config.database.mysql);
+
+/**
+ * @param func トランザクション処理を行う場合はこの中で書いてください。値を返す場合は必ず配列で返して下さい。
+ */
 const transaction = async (func: Function): Promise<any[]> => {
     let response: any[] = [];
     const connection = mySqlConnector.getConnection();
     try{
 
-        let checker: any[] = [];
+        let checker: any = null;
 
         (await connection).beginTransaction();
         checker = await func();
-        (await connection).commit();
 
-        if(checker.length > 0){
+        if(checker && Array.isArray(checker)){
             response = checker;
         }
+
+        (await connection).commit();
 
     }catch(e){
         (await connection).rollback();
