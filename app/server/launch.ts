2f13d935e869042bd926a0188edcbf65138acc16
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-12 18:11:46 +0900

    corsの設定を設定ファイルで出来るように

--- app/server/launch.ts
+++ app/server/launch.ts
@@ -1,41 +1,47 @@
 import express, { NextFunction } from 'express';
 import { Socket } from 'socket.io';
 import route from './Route';
+import Config from './config';
+import logger from './Domain/Utility/logger';
+
+logger.info(Config.system.cors);
 
 const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
 const session = require('express-session')({
-	secret: 'St5Q3wPtv4TJ',
-	resave: false,
-	saveUninitialized: true
+  secret: 'St5Q3wPtv4TJ',
+  resave: false,
+  saveUninitialized: true
 });
 const socketListener = require('./Listener');
-const io = require('socket.io')(server,{
-  cors:{
-    origin: ['http://localhost:8080','http://localhost:8081','http://localhost:8082'],
-    methods: ['GET','POST'],
+const io = require('socket.io')(server, {
+  cors: {
+    origin: Config.system.cors,
+    methods: ['GET', 'POST'],
     credentials: true
   }
 });
 app.use(cors({
-  origin: ['http://localhost:8080','http://localhost:8081','http://localhost:8082'],
+  origin: Config.system.cors,
   credentials: true
 }));
 app.use(express.json());
 app.use(session);
-io.use((socket: Socket,next: NextFunction)=>{
-  session(socket.request,{},next);
+io.use((socket: Socket, next: NextFunction) => {
+  session(socket.request, {}, next);
 });
 //app.use(express.static(path.join(__dirname, '../../dist')));
 socketListener(io);
 //expressルート定義
 route(app);
-const launch =(port:number) => {
-    server.listen(port, () => {
-        console.log('Server listening at port %d', port);
-    });
+const launch = (port: number) => {
+  server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+  });
 }
+
+
 
 
 
