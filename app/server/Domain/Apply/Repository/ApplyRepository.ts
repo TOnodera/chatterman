bb4d1a4af4563bd81e3bf35f1cf3a077f03bc1dd
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-12 22:51:36 +0900

    unique_idに紐づくmesage_polymorphic返すgetMessageOptions追加

--- app/server/Domain/Apply/Repository/ApplyRepository.ts
+++ app/server/Domain/Apply/Repository/ApplyRepository.ts
@@ -1,4 +1,5 @@
 import { Pool } from "mysql2/promise";
+import logger from "../../../Domain/Utility/logger";
 import Exception from "../../Exception/Exception";
 
 class ApplyRepository{
@@ -10,8 +11,9 @@ class ApplyRepository{
     }
 
     async apply(target_id: string,user_id: string): Promise<string>{
-        const [row]: any[] = await this.connector.query('INSERT INTO requests SET target_user = ? ,request_user = ?, is_accept = ?, accept_notified = ?,created_at = NOW();',[target_id,user_id,APPLY_REACTION.IS_ACCEPT_UNTREATED,APPLY_SENDER_NOTICE.IS_NOTIFIED_YET]);
-        return row.insertId;
+        const [result]: any[] = await this.connector.query('INSERT INTO requests SET target_user = ? ,request_user = ?, is_accept = ?, accept_notified = ?,created_at = NOW();',[target_id,user_id,APPLY_REACTION.IS_ACCEPT_UNTREATED,APPLY_SENDER_NOTICE.IS_NOTIFIED_YET]);
+        logger.debug(result.insertId);
+        return result.insertId;
     }
 
     async hasAlreadyRequested(target_id: string,user_id: string): Promise<boolean>{
@@ -24,8 +26,8 @@ class ApplyRepository{
         return rows.length > 0;
     }
 
-    async getUserId(apply_id: string): Promise<string>{
-        const [rows]: any[] = await this.connector.query("SELECT request_user FROM requests WHERE id = ?",[apply_id]);
+    async getUserId(polymorphic_id: number): Promise<string>{
+        const [rows]: any[] = await this.connector.query("SELECT request_user FROM requests WHERE id = ?",[polymorphic_id]);
         if(rows.length > 0){
             return rows[0].request_user;
         }        
@@ -33,23 +35,31 @@ class ApplyRepository{
     }
 
     async registeApplyReaction(unique_id: string, reaction: number): Promise<boolean> {
-        const [row]: any[] = await this.connector.query('SELECT message_id,polymorphic_table as table FROM message_polymorphics WHERE id = ? ',[unique_id]);
-        const message_id = row.message_id;
-        if(row.length == 1 && row.table == 'requests'){ //ハードコーディングなおす
-            const [row]: any[] = await this.connector.query('UPDATE requests SET is_accept = ? WHERE message_id = ? ',[reaction,message_id]);
-            return row.affectedRows == 1;
+        const [rows]: any[] = await this.connector.query('SELECT message_id,polymorphic_table as table FROM message_polymorphics WHERE id = ? ',[unique_id]);
+        const message_id = rows[0].message_id;
+        if(rows.length == 1 && rows[0].table == PolymorphicTables.requests) {
+            const [result]: any[] = await this.connector.query('UPDATE requests SET is_accept = ? WHERE message_id = ? ',[reaction,message_id]);
+            return result.affectedRows == 1;
         }
         throw new Exception("unique_idに一致するデータが見つかりませんでした。");
     }
 
     async isThePerson(unique_id: string, user_id: string): Promise<boolean> {
-        const [row]: any[] = await this.connector.query('SELECT * FROM message_polymorphics WHERE id = ? ',[unique_id]);
-        const message_id = row.message_id;
-        const options: MessageOptions = {polymorphic_table: row.polymorphic_table,unique_id: row.unique_id};
+        const [rows]: any[] = await this.connector.query('SELECT * FROM message_polymorphics WHERE id = ? ',[unique_id]);
+        const message_id = rows[0].message_id;
         
-        if(row.length == 1){
-            const [rows]: any[] = await this.connector.query(`SELECT * FROM  ${options.polymorphic_table} WHERE message_id = ? AND user_id = ?`,[message_id,user_id]);
-            return rows.length == 1;
+        if(rows.length == 1 && rows[0].table == PolymorphicTables.requests){
+            const [results]: any[] = await this.connector.query(`SELECT * FROM  ${rows[0].table} WHERE message_id = ? AND user_id = ?`,[message_id,user_id]);
+            return results.length == 1;
+        }
+        throw new Exception("unique_idに一致するデータが見つかりませんでした。");
+    }
+
+    async getMessageOptions(unique_id: number): Promise<any>{
+        const [rows]: any[] = await this.connector.query('SELECT * FROM message_polymorphics WHERE unique_id = ?',[unique_id]);
+        logger.debug('getMessageOptions',rows);
+        if(rows.length > 0){
+            return rows[0];
         }
         throw new Exception("unique_idに一致するデータが見つかりませんでした。");
     }
