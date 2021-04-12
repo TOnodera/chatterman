c23d94bd159dbe7b025fff1b5c9d5e779ed0d03d
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-12 15:09:37 +0900

    変更完了

--- app/server/config/system.config.ts
+++ app/server/config/system.config.ts
@@ -1,4 +1,5 @@
 export default {
     superuser: process.env.SYSTEM_USER,
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    cors: process.env.ARROWED_CORS?.split(',')
 };
