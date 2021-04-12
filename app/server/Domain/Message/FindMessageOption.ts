eafe50c72138a4b2055f3a7d04b52f0bcf93801f
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-12 22:53:07 +0900

    微修正

--- app/server/Domain/Message/FindMessageOption.ts
+++ app/server/Domain/Message/FindMessageOption.ts
@@ -1,6 +1,7 @@
 import ApplyRepositoryFactory from "../Apply/Factory/ApplyRepositoryFactory";
 import ApplyRepository from "../Apply/Repository/ApplyRepository";
 import Exception from "../Exception/Exception";
+import logger from "../Utility/logger";
 import MessageOptionsRepositoryFactory from "./Factory/MessageOptionsRepositoryFactory";
 import MessageOptionsRepository from "./Repository/MessageOptionsRepository";
 
@@ -23,14 +24,20 @@ class FindMessageOption{
      * message_polymorphicsのunique_idから必要な情報を取得してクライアントに渡すオブジェクトを作る
      * 追加機能がメッセージに関連する追加機能が増えたら戻り値のoptionに型を追加する
      */
-    async getMessageOptionById(unique_id: string): Promise<ApproveOptions>{
-        const options: MessageOptions = await this.messageOptionsRepository.get(unique_id);
-        switch(options.polymorphic_table){
+    async getMessageOptionById(unique_id: number): Promise<ApproveOptions>{
+
+        const {polymorphic_table,polymorphic_id} = await this.messageOptionsRepository.get(unique_id);
+
+        switch(polymorphic_table){
+
             case 'requests':
-                const user_id: string = await this.applyRepository.getUserId(unique_id);
-                return {unique_id: unique_id,user_id: user_id} as ApproveOptions;    
+                const user_id: string = await this.applyRepository.getUserId(polymorphic_id);
+                const options: ApproveOptions = {unique_id: unique_id,user_id: user_id};
+                return  options;
+
             default:
                 throw new Exception("message_polymorphics.polymorphic_tableに不正な値が設定されました。");
+                
         }
     }
 }
