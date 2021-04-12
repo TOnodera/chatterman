fb628ecde7536aa50ec7078994f0b8f3835f3402
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-13 02:39:25 +0900

    unionに変更と微修正

--- app/server/enum/enum.ts
+++ app/server/enum/enum.ts
@@ -1,15 +1,24 @@
-const enum PolymorphicTables {
-    requests = 'requests'
-};
 
-const enum APPLY_REACTION {
-    IS_ACCEPT_UNTREATED = 0, //未処理
-    IS_ACCEPT_ARROW = 1, //OK
-    IS_ACCEPT_DENY = 2 //NO
-};
+const PolymorphicTables = {
+    requests: 'requests'
+} as const;
+type PolymorphicTables = typeof PolymorphicTables[keyof typeof PolymorphicTables];
 
-const enum APPLY_SENDER_NOTICE {
-    IS_NOTIFIED_YET = 1, //未送信
-    IS_NOTIFIED_DONE = 2 //送信済
-};
+const APPLY_SENDER_NOTICE = {
+    IS_NOTIFIED_YET: 1, //未送信
+    IS_NOTIFIED_DONE: 2 //送信済
+} as const;
+type APPLY_SENDER_NOTICE = typeof APPLY_SENDER_NOTICE[keyof typeof APPLY_SENDER_NOTICE];
 
+const APPLY_REACTION = {
+    IS_ACCEPT_UNTREATED: 0, //未処理
+    IS_ACCEPT_ARROW: 1, //OK
+    IS_ACCEPT_DENY: 2 //NO
+} as const;
+type APPLY_REACTION = typeof APPLY_REACTION[keyof typeof APPLY_REACTION];
+
+export {
+    PolymorphicTables,
+    APPLY_SENDER_NOTICE,
+    APPLY_REACTION
+}
