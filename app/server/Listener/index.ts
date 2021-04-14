bf2f045d8d8ab40c49050af6234b9867c911fd06
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-14 13:25:48 +0900

    ユーザーリスナからルームリスナ切り出し

--- app/server/Listener/index.ts
+++ app/server/Listener/index.ts
@@ -3,12 +3,14 @@ const chatListener = require('./chatListener');
 const userListener = require('./userListener');
 const applyListener = require('./applyListener');
 const serverErrorListener = require("./serverErrorListener");
+const roomListener = require('./roomListener');
 
 module.exports = (io:any) => {
     io.on('connection',(socket: Socket)=>{
         chatListener(socket);
         userListener(socket);
         applyListener(socket);
+        roomListener(socket);
         serverErrorListener(socket);
     });
 }
---
+++ app/server/Listener/roomListener.ts
@@ -0,0 +1,28 @@
+import RoomController from "../Domain/Controller/RoomController";
+import UserController from "../Domain/Controller/UserController";
+import { ROOM_TYPE } from "../Enum/Enum";
+import { Socket } from "socket.io";
+
+module.exports = (socket: Socket) => {
+
+    const roomController = new RoomController(socket);
+
+    //ルーム作成
+    const createRoom = async (name: string, user_id: string) => {
+        await roomController.createRoom(name, user_id, ROOM_TYPE.talkroom);
+    };
+
+    //ダイレクトメッセージルーム情報送信
+    const requireUsers = async (user_id: string) => {
+        await UserController.getDirectMessageRoomInfo(user_id, socket);
+    }
+
+    //入室権限があるルーム情報のデータを送信
+    const requireRooms = async (user_id: string) => {
+        await roomController.getTalkRooms(user_id);
+    };
+
+    socket.on('user:create-room',createRoom);
+    socket.on('user:require-members',requireUsers);
+    socket.on('user:require-rooms',requireRooms);
+}
