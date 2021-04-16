6a4035f048444d9144a832a9a960fdf8abfcccc9
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-16 09:55:57 +0000

    ベタ書きで動くの確認

--- app/server/launch.ts
+++ app/server/launch.ts
@@ -14,57 +14,54 @@ const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
 const session = require('express-session')({
-  secret: 'St5Q3wPtv4TJ',
-  resave: false,
-  saveUninitialized: true
+	secret: 'St5Q3wPtv4TJ',
+	resave: false,
+	saveUninitialized: true
 });
 const socketListener = require('./Listener');
 const io = require('socket.io')(server, {
-  cors: {
-    origin: Config.system.cors,
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+	cors: {
+		origin: Config.system.cors,
+		methods: ['GET', 'POST'],
+		credentials: true
+	}
 });
 app.use(
-  cors({
-    origin: Config.system.cors,
-    credentials: true
-  })
+	cors({
+		origin: Config.system.cors,
+		credentials: true
+	})
 );
 app.use(express.json());
 io.use((socket: Socket, next: NextFunction) => {
-  session(socket.request, {}, next);
+	session(socket.request, {}, next);
 });
 app.use(session);
 //ソケットのガード設定
 io.use(async (socket: Socket, next: NextFunction) => {
-  //@ts-ignore
-  logger.debug(socket.request);
-  /*
-try {
-  logger.debug("in auth middleware...");
-  logger.debug(socket.request.session);
-  if (await loginManager.getAfterLoginManager(socket).authenticate(socket.request.session.credentials)) {
-    logger.debug("next()");
-    next();
-  } else {
-    throw new AuthenticationException("ログインして下さい。");
-  }
-} catch (e) {
-  SocketExceptionHandler.handle(e, socket);
-}
-*/
-  next();
+
+	try {
+		logger.debug("ソケット通信の認証開始　セッション内容 -> ", socket.request.session);
+		if (await loginManager.getAfterLoginManager(socket).authenticate(socket.request.session.credentials)) {
+			logger.debug("認証成功 -> ", socket.request.session);
+			next();
+		} else {
+			throw new AuthenticationException("ログインして下さい。");
+		}
+	} catch (e) {
+		logger.debug("認証失敗 -> ", socket.request.session);
+		SocketExceptionHandler.handle(e, socket);
+	}
+	next();
 });
 //app.use(express.static(path.join(__dirname, '../../dist')));
 socketListener(io);
 //expressルート定義
 route(app);
 const launch = (port: number) => {
-  server.listen(port, () => {
-    console.log('Server listening at port %d', port);
-  });
+	server.listen(port, () => {
+		console.log('Server listening at port %d', port);
+	});
 };
 
 export default launch;
