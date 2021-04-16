d25155fc7d35d79374c43e2fa9076a60a9dfdbdc
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-16 07:31:50 +0000

    サーバーサイドは自動フォーマット設定した。フロントあとでやってみる

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    //'plugin:vue/vue3-essential',
-    //'@vue/standard',
-    //'@vue/typescript/recommended'
-    
-  ],
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    ecmaVersion: 2015,
-    sourceType: "module",
-    'parser': '@typescript-eslint/parser'
-  },
-  rules: {
-    //'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    //'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    semi: ['error', 'always']
-  }
-};
+    root: true,
+    parserOptions: {
+      parser: 'babel-eslint',
+      sourceType: 'module',
+    },
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'airbnb-base',
+      'plugin:prettier/recommended',
+      'plugin:vue/recommended',
+      'prettier/vue',
+    ],
+    plugins: ['vue', 'prettier'],
+    rules: {
+      'global-require': 0,
+      'import/no-unresolved': 0,
+      'no-param-reassign': 0,
+    }
+  };
