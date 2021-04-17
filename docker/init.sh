8064ffe7108e0b164f4275b91298f87b73e226cc
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-17 13:19:52 +0900

    init.sh移動&パスワード生成処理をハッシュに変更

--- docker/init.sh
+++ docker/init.sh
@@ -2,14 +2,14 @@
 
 #初期化用SQL生成ファイル
 #docker-compose を使う前にこのスクリプトを実行して下さい。
-#実行するとinitdb.d内にsqlが出力されてコンポーネント起動時に自動的に初期化されます。
+#実行するとinitdb.d内にsqlが出力されてコンポーネント起動時に自動的に初期化さ.envにホストマシンのユーザー情報が書き込まれます。
 
 #設定ファイル読み込み
-. ../../.env
+. ./.env
 
-DDL_SQL=$(cat ddl.sql)
+DDL_SQL=$(cat ./mysql/init/ddl.sql)
 
-ENCRYPTED=$(echo "$SYSTEM_PASSWORD" | openssl enc -e -des -base64 -k PASSPHRASE)
+ENCRYPTED=$(echo "$SYSTEM_PASSWORD" | md5sum )
 
 DML_SQL=$(cat << SQLTXT
 
@@ -23,7 +23,9 @@ SQLTXT
 
 SQL=$DDL_SQL$DML_SQL
 
-echo -n > ../initdb.d/init.sql;
-echo $SQL >> ../initdb.d/init.sql
+echo -n > ./mysql/initdb.d/init.sql;
+echo $SQL >> ./mysql/initdb.d/init.sql
 
+echo "HOST_USER_ID=$(id -u $USER)" >> .env
+echo "HOST_GROUP_ID=$(id -g $USER)" >> .env
 
--- docker/set-enviroment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "HOST_USER_ID=$(id -u $USER)" >> .env
-echo "HOST_GROUP_ID=$(id -g $USER)" >> .env
