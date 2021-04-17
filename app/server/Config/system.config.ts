daecf820a481dbe18c48e2db33eea693b9f89190
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-04-17 05:55:09 +0000

    baseUrlはクライアント側間違え修正。

--- app/server/Config/system.config.ts
+++ app/server/Config/system.config.ts
@@ -1,6 +1,5 @@
 export default {
     superuser: process.env.SYSTEM_USER,
-    baseUrl: 'http://localhost:3000',
     cors: process.env.ARROWED_CORS?.split(','),
     cookie_secret: process.env.COOKIE_SECRET
 };
